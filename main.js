40a368a6089e2fa9def52c8b48deb248d567333c
AndreaDuh <xAsianRejectx@gmail.com>
2015-10-28 01:30:18 -0700

    Added a prompt for users to input a number to fizzbuzz.

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-for (var i=1; i <= 100; i++) {
-	if(i % 15 === 0){
-		document.write("FizzBuzz" + "<br>");
+$(document).ready(function() {
+	var input = +`${prompt('Choose a number to FizzBuzz to!')}`;
+	for (var i=1; i <= input; i++) {
+		if(i % 15 === 0){
+			document.write("FizzBuzz" + '<br>');
+		}
+		else if(i % 3 === 0){
+			document.write("Fizz" + '<br>');
+		}
+		else if(i % 5 === 0) {
+			document.write("Buzz" + '<br>');
+		}
+		else {
+			document.write(i + '<br>');
+		}
 	}
-	else if(i % 3 === 0){
-		document.write("Fizz" + "<br>");
-	}
-	else if(i % 5 === 0) {
-		document.write("Buzz" + '<br>');
-	}
-	else {
-		document.write(i + '<br>');
-	}
-};
+});
+
+
+
 
 
